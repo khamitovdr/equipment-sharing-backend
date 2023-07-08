3c7f08bde982c2366cd6594136ce2c57ebecbf7a
denis240997 <hamitov.97@mail.ru>
2023-07-08 17:58:00 +0300

    Testing scripts remove

--- restart/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker compose down -v
-sudo rm -r project/migrations/
-docker compose up -d --build
-sleep 3
-docker compose exec web aerich init-db
-
-./project/env/bin/python ./restart/init_script.py
