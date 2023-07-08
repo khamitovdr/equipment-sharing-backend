19eca583d6b9cbaed1160cb7a05920ea9455a802
denis240997 <hamitov.97@mail.ru>
2023-07-08 17:59:01 +0300

    Mock organizations

--- project/app/services/organizations.py
+++ project/app/services/organizations.py
@@ -27,36 +27,53 @@ async def get_or_create_organization_by_inn(inn: str) -> Organization:
     return organization
 
 
+# async def get_organization_data_by_code(query: str) -> DadataResponseSchema:
+#     """Returns organization data by code from DaData API."""
+#     log.info(f"Getting organization data by code {query} from DaData API...")
+#     async with DadataAsync(DADATA_TOKEN) as dadata:
+#         result = await dadata.find_by_id("party", query, status="ACTIVE", count=1)
+
+#     if not result:
+#         log.error(f"Organization not found by code {query}")
+#         organization_not_found_exception = HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail="Organization not found",
+#         )
+#         raise organization_not_found_exception
+    
+#     result = result[0]["data"]
+#     log.info(f"Organization data by code {query} from DaData API received successfully")
+#     response = DadataResponseSchema(
+#         short_name=result["name"]["short_with_opf"],
+#         full_name=result["name"]["full_with_opf"],
+#         ogrn=result["ogrn"],
+#         inn=result["inn"],
+#         kpp=result["kpp"],
+#         registration_date=result["state"]["registration_date"],
+#         # authorized_capital_k_rubles=result["capital"]["value"],
+#         legal_address=result["address"]["value"],
+#         manager_name=result["management"]["name"],
+#         # main_activity=okveds[result["okved"]],
+#         main_activity=result["okved"],
+#     )
+#     return response
+
+
 async def get_organization_data_by_code(query: str) -> DadataResponseSchema:
     """Returns organization data by code from DaData API."""
-    log.info(f"Getting organization data by code {query} from DaData API...")
-    async with DadataAsync(DADATA_TOKEN) as dadata:
-        result = await dadata.find_by_id("party", query, status="ACTIVE", count=1)
-
-    if not result:
-        log.error(f"Organization not found by code {query}")
-        organization_not_found_exception = HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Organization not found",
-        )
-        raise organization_not_found_exception
-    
-    result = result[0]["data"]
-    log.info(f"Organization data by code {query} from DaData API received successfully")
-    response = DadataResponseSchema(
-        short_name=result["name"]["short_with_opf"],
-        full_name=result["name"]["full_with_opf"],
-        ogrn=result["ogrn"],
-        inn=result["inn"],
-        kpp=result["kpp"],
-        registration_date=result["state"]["registration_date"],
-        # authorized_capital_k_rubles=result["capital"]["value"],
-        legal_address=result["address"]["value"],
-        manager_name=result["management"]["name"],
-        # main_activity=okveds[result["okved"]],
-        main_activity=result["okved"],
+    log.info(f"Getting organization data by code {query} from mock DaData API...")
+    log.info(f"Organization data by code {query} from mock DaData API received successfully")
+    return DadataResponseSchema(
+        short_name=f"mock_short_name_{query}",
+        full_name=f"mock_name_{query}",
+        ogrn=f"mock_ogrn_{query}",
+        inn=f"{query}",
+        kpp=f"mock_kpp_{query}",
+        registration_date="0000000000000",
+        legal_address="mock_legal_address",
+        manager_name="mock_manager_name",
+        main_activity="01",
     )
-    return response
 
 
 async def init_activities_db_table():
