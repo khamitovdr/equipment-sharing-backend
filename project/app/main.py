8e82bf007cd76dedc24e7603cb808a76b7da312c
denis240997 <hamitov.97@mail.ru>
2023-08-04 13:50:22 +0300

    Scheduler embedded in app event loop

--- project/app/main.py
+++ project/app/main.py
@@ -1,20 +1,13 @@
-import asyncio
 import logging
-import os
-import sys
 
-import uvicorn
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.staticfiles import StaticFiles
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(parent_dir)
-
+from app import scheduler
 from app.api import equipment, notifications, orders, organizations, token, users
 from app.db import init_db
 from app.db_signals import files_signals, orders_signals  # noqa: F401
-from app.scheduler import app as app_rocketry
 
 log = logging.getLogger("uvicorn")
 
@@ -36,7 +29,20 @@ def create_application() -> FastAPI:
 app = create_application()
 
 
-# swagger ui dark theme
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+    await scheduler.start()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
+    await scheduler.stop()
+
+
+# Swagger ui dark theme
 @app.get("/docs-dark", include_in_schema=False)
 async def custom_swagger_ui_html_cdn():
     return get_swagger_ui_html(
@@ -45,17 +51,6 @@ async def custom_swagger_ui_html_cdn():
         # swagger_ui_dark.css CDN link
         swagger_css_url="https://cdn.jsdelivr.net/gh/Itz-fork/Fastapi-Swagger-UI-Dark/assets/swagger_ui_dark.min.css",
     )
-
-
-@app.on_event("startup")
-async def startup_event():
-    log.info("Starting up...")
-    init_db(app)
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    log.info("Shutting down...")
 
 
 @app.get("/fill_db")
@@ -68,34 +63,3 @@ async def init_activities_db():
     await init_equipment_categories_db_table()
 
     return {"message": "OK"}
-
-
-class Server(uvicorn.Server):
-    """Customized uvicorn.Server
-
-    Uvicorn server overrides signals and we need to include
-    Rocketry to the signals."""
-
-    def handle_exit(self, sig: int, frame) -> None:
-        app_rocketry.session.shut_down()
-        return super().handle_exit(sig, frame)
-
-
-async def main():
-    "Run scheduler and the API"
-
-    # https://www.uvicorn.org/settings/
-    server = Server(
-        config=uvicorn.Config(
-            app, workers=1, loop="asyncio", host="0.0.0.0", port=8000, reload=True, reload_dirs=["app"]
-        )
-    )
-
-    api = asyncio.create_task(server.serve())
-    sched = asyncio.create_task(app_rocketry.serve())
-
-    await asyncio.wait([sched, api])
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
--- project/app/scheduler.py
+++ project/app/scheduler.py
@@ -1,4 +1,23 @@
+import asyncio
+import logging
+
 from rocketry import Rocketry
 
-# Creating the Rocketry app
+log = logging.getLogger("uvicorn")
+
 app = Rocketry(config={"task_execution": "async"})
+
+
+async def start():
+    """Start the scheduler"""
+    log.info("Starting up Rocketry scheduler")
+
+    # hook the rocketry scheduler into the FastAPI event loop
+    fastapi_event_loop = asyncio.get_running_loop()
+    fastapi_event_loop.create_task(app.serve())
+
+
+async def stop():
+    """Stop the scheduler"""
+    log.info("Stopping Rocketry scheduler")
+    app.session.shut_down()
